0925f80ee92eac4dc768ec0b32922eeb400b982a
blacksagres <geraldo.nogueirabs@gmail.com>
2025-07-20 12:34:01 +0200

    remove beam particle variable

--- characters/player/laser_sight.gd
+++ characters/player/laser_sight.gd
@@ -4,7 +4,6 @@ extends RayCast2D
 @export var max_length := 1400.0
 
 @onready var laser_line :Line2D = $LaserLine
-@onready var beam_particles := $BeamParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
